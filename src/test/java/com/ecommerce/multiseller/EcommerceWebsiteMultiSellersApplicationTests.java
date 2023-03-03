b032a96c65bb1938eff7504c6f8ccd91065f9971
Rachid Benkitou <rachidbenkitou1@gmail.com>
2023-03-03 14:58:45 +0100

    first commit

---
+++ src/test/java/com/ecommerce/multiseller/EcommerceWebsiteMultiSellersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.multiseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebsiteMultiSellersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
