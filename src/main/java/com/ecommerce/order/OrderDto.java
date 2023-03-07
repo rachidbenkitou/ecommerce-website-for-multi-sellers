89bb17881425a4c7f22e76efb74104d2ce31cec3
ysfmh14 <ysfmh2002@gmail.com>
2023-03-07 19:40:33 +0100

    classe order done

--- src/main/java/com/ecommerce/order/OrderDto.java
+++ src/main/java/com/ecommerce/order/OrderDto.java
@@ -3,18 +3,12 @@ package com.ecommerce.order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.Date;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
-public class Order {
-    @Id
+public class OrderDto {
     private long orderId;
     private Date date;
 }
