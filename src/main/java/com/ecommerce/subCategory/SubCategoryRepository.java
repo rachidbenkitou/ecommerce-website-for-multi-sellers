874fee62e581fb76d8e70db3ac17452762cba2e0
OsamaElzero <Mouhcine2001k@gmail.com>
2023-03-30 03:08:27 +0100

    add method findByCtegoryName in repository

--- src/main/java/com/ecommerce/subCategory/SubCategoryRepository.java
+++ src/main/java/com/ecommerce/subCategory/SubCategoryRepository.java
@@ -4,11 +4,13 @@ import com.ecommerce.category.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubCategoryRepository extends MongoRepository<SubCategory, String> {
     boolean existsBySubCategoryName(String name);
     void deleteSubCategoryBySubCategoryName(String SubCategoryName);
     //List<SubCategory> findSuCategoriesBySuCategoryNameLikeIgnoreCase(String categoryName);
     List<SubCategory> findSubCategoriesBySubCategoryNameLikeIgnoreCase(String categoryName);
+    Optional<SubCategory> findBySubCategoryName(String subCategoryName);
 
 }
